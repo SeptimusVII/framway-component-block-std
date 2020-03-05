e1e10410d984e877c0c1a1368f426a675a4e05b0
Quentin Vansteene <vansteene.quentin@gmail.com>
2020-03-05 15:59:31 +0100

    Initial commit

---
+++ block-std.js
@@ -0,0 +1,11 @@
+var BlockStd = Object.getPrototypeOf(app).BlockStd = new Component("block-std");
+BlockStd.debug = true;
+BlockStd.createdAt      = "2.0.0";
+BlockStd.lastUpdate     = "2.0.0";
+BlockStd.version        = "1";
+// BlockStd.factoryExclude = true;
+// BlockStd.loadingMsg     = "This message will display in the console when component will be loaded.";
+
+// BlockStd.prototype.onCreate = function(){
+  // do thing after element's creation
+// }
