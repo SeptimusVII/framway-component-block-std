60fd78ebdb669992134e989e89fd066cf979a00d
Quentin Vansteene <vansteene.quentin@gmail.com>
2020-04-15 12:03:32 +0200

    component is now exported as a separate module, that properly requires app to work

--- block-std.js
+++ block-std.js
@@ -1,11 +1,14 @@
-var BlockStd = Object.getPrototypeOf(app).BlockStd = new Component("block-std");
-// BlockStd.debug = true;
-BlockStd.createdAt      = "2.0.0";
-BlockStd.lastUpdate     = "2.0.0";
-BlockStd.version        = "1";
-// BlockStd.factoryExclude = true;
-// BlockStd.loadingMsg     = "This message will display in the console when component will be loaded.";
+module.exports = function(app){
+	var BlockStd = Object.getPrototypeOf(app).BlockStd = new app.Component("block-std");
+	// BlockStd.debug = true;
+	BlockStd.createdAt      = "2.0.0";
+	BlockStd.lastUpdate     = "2.0.0";
+	BlockStd.version        = "1";
+	// BlockStd.factoryExclude = true;
+	// BlockStd.loadingMsg     = "This message will display in the console when component will be loaded.";
 
-// BlockStd.prototype.onCreate = function(){
-  // do thing after element's creation
-// }
+	// BlockStd.prototype.onCreate = function(){
+	  // do thing after element's creation
+	// }
+	return BlockStd;
+}
