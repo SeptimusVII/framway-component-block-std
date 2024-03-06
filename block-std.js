a3e99bd4cd35641f86f0f606652954199712ab5b
Quentin Vansteene <vansteene.quentin@gmail.com>
2024-03-06 16:31:42 +0100

    wrap styles into a mixin. make a %block-std placeholder to be extended if needed

--- block-std.js
+++ block-std.js
@@ -2,8 +2,8 @@ module.exports = function(app){
 	var BlockStd = Object.getPrototypeOf(app).BlockStd = new app.Component("block-std");
 	// BlockStd.debug = true;
 	BlockStd.createdAt      = "2.0.0";
-	BlockStd.lastUpdate     = "2.2.1";
-	BlockStd.version        = "1.0.1";
+	BlockStd.lastUpdate     = "2.4.5";
+	BlockStd.version        = "1.1.0";
 	// BlockStd.factoryExclude = true;
 	// BlockStd.loadingMsg     = "This message will display in the console when component will be loaded.";
 
