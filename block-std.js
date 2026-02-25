df1e8feed98c4511d652459f8882b763ac5cfa41
SeptimusVII <vansteene.quentin@gmail.com>
2026-02-25 18:53:26 +0100

    sets debug to false

--- block-std.js
+++ block-std.js
@@ -1,7 +1,7 @@
 module.exports = function(){
     let BlockStd = Object.getPrototypeOf(fw).BlockStd = class BlockStd extends fw.Component{
         static {
-            this.debug = true;
+            this.debug = false;
             this.createdAt  = "3.0.0";
             this.lastUpdate = "3.0.0";
             this.version = "2.0.0";
